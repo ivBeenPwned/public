5b73aae47b96f8a34fae357b1185ed5f02f8cfe1
ivBeenPwned <100182360+ivBeenPwned@users.noreply.github.com>
2025-08-27 14:27:00 -0300

    Update zap-auto.sh

--- zap-auto.sh
+++ zap-auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $(id -u) == 0 ]]; then
-	echo -e "O script NÃO deve ser executado como root, pois:\n\n - Poderá ocorrer erros ao executar certos escaneamentos como AjaxSpider devido a limitação de uso com o Firefox para usuários que possuem permissiões Administrativas (root) no dispostivo\n - Poderá comprometer sua máquina em determinadas ocasiões.\n - Entre outras recomendações como o princípio do privilégio mínimo e etc..."
+	echo -e "O script NÃO deve ser executado como root, pois:\n\n - Poderá ocorrer erros ao executar certos escaneamentos como AjaxSpider devido a limitação de uso com o Firefox para usuários que possuem permissiões Administrativas (root) no dispostivo"
 	exit 1
 fi
 
