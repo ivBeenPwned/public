bec7b21baaa558ecefc517dc1d5ef258cb5aa3ba
ivBeenPwned <100182360+ivBeenPwned@users.noreply.github.com>
2025-08-30 23:19:39 -0300

    Update basic-enum.sh

--- basic-enum.sh
+++ basic-enum.sh
@@ -60,7 +60,7 @@ cmds=(
 	"/usr/bin/nikto -h __DOMAIN__ -maxtime 1h -Format json -output ${PWD}/Scans/__DOMAIN__/nikto.json"
 	"/usr/bin/whatweb --plugins=Apache,Nginx,Cloudflare,F5-BIGIP,Title,X-Powered-By,PHP,Python,Perl,Ruby,WordPress,Joomla,Drupal,Magento,Shopify,Laravel,Symfony,Django,Jetty,OpenResty,Google-Analytics,jQuery,Bootstrap,Modernizr,HTML5,HTTPServer,Meta-Author __DOMAIN__ --log-verbose=${PWD}/Scans/__DOMAIN__/WhatWeb.txt"
 	"/usr/bin/nuclei -target __DOMAIN__ -json-export ${PWD}/Scans/__DOMAIN__/nuclei.json -no-color -silent"
-	"/usr/bin/dirsearch -u __DOMAIN__ -w ${Seclist_raft} --no-color --random-agent --max-time=3600 --format=plain --output=${PWD}/Scans/__DOMAIN__/dirsearch.txt"
+	"/usr/bin/dirsearch -u __DOMAIN__ -w ${Seclist_raft} --no-color -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0' --max-time=3600 --format=plain --output=${PWD}/Scans/__DOMAIN__/dirsearch.txt"
 )
 
 MAX_JOBS=4
